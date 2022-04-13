c3388eb242d3293c14ec950f4c0cf961a3cd8ca6
otherthanstandard <97838530+otherthanstandard@users.noreply.github.com>
2022-04-13 17:40:55 +0100

    Add files via upload

--- pages/index.tsx
+++ pages/index.tsx
@@ -6,7 +6,6 @@ import Link from 'next/link'
 // Components
 import Route from 'components/route'
 import MapBox from 'components/mapbox'
-import Button from 'components/button'
 import RoutePage from 'components/routepage'
 import Select from 'components/select'
 
@@ -27,7 +26,6 @@ type RoutesProps = {
 
 const Home = ({ routes, queryRoute }: RoutesProps) => {
   const [sorting, setSorting] = useState('added')
-  const [filter, setFilter] = useState('all')
   const aside = useRef<HTMLElement>()
   const currentRoute = routes.find(route => route.slug === queryRoute)
   const randomRoute = routes[Math.floor(Math.random() * routes.length)]
@@ -65,21 +63,17 @@ const Home = ({ routes, queryRoute }: RoutesProps) => {
           animate={
             queryRoute
               ? {
-                  x: '0%',
-                  scale: 1,
+                  x: '-30%',
+                  scale: 0.95,
                   transitionEnd: {
                     display: 'none',
                   },
                 }
               : { x: 0, scale: 1, display: 'block' }
           }
-          transition={{ ease: 'easeOut', duration: 0 }}
-        >          <Nav />
-          <nav className="flex justify-end">
-            <Button onClick={() => window.open('mailto:samuelkraft@me.com?subject=🏃‍♀️ I want to add a route to Trail Runner!')}>
-              Add Route
-            </Button>
-          </nav>
+          transition={{ ease: 'easeOut', duration: 0.2 }}
+        >
+          <Nav />
           <header className="py-16 text-center">
             <img src="/logo.svg" alt="Trail Router logotype" className="mx-auto mb-3" />
             <p className="text-forest-darkest">
@@ -89,7 +83,7 @@ const Home = ({ routes, queryRoute }: RoutesProps) => {
           </header>
           <section>
             <div className="sticky z-10 flex justify-between px-5 py-4 -mx-5 bg-blur -top-5">
-              <h1 className="text-2xl font-bold text-forest-darkest"></h1>
+              <h1 className="text-2xl font-bold text-forest-darkest">All routes</h1>
               <div className="flex">
                 <Link href={{ query: { route: randomRoute.slug } }}>
                   <a
@@ -116,13 +110,6 @@ const Home = ({ routes, queryRoute }: RoutesProps) => {
                     </svg>
                   </a>
                 </Link>
-                <a>
-                <Select value={filter} onChange={e => setFilter(e.target.value)}>
-                   <option value="all">All</option>
-                   <option value="swimrun">Swimrun</option>
-                   <option value="running">Running</option>
-                 </Select>
-                </a>
                 <Select value={sorting} onChange={e => setSorting(e.target.value)}>
                   <option value="added">Recently added</option>
                   <option value="rating">Rating</option>
@@ -133,20 +120,9 @@ const Home = ({ routes, queryRoute }: RoutesProps) => {
               </div>
             </div>
             <ol>
-                     {routes
-                 .filter(route => {
-                   if (filter === 'swimrun' && !route.swimrun) {
-                     return false
-                   }
-                   if (filter === 'running' && route.swimrun) {
-                     return false
-                   }
-                   return true
-                 })
-                 .sort(sortRoutes)
-                 .map(route => (
-                   <Route key={route.slug} route={route} />
-                 ))}
+              {routes.sort(sortRoutes).map(route => (
+                <Route key={route.slug} route={route} />
+              ))}
             </ol>
           </section>
           <footer className="text-center text-gray-500">
